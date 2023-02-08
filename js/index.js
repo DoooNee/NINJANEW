3a0b257724d6b48e66798a13fb94e781d541bd46
DoooNee <88998905+DoooNee@users.noreply.github.com>
2023-02-08 16:05:49 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+// active char
+var chars = document.querySelectorAll(".img_char");
+chars.forEach((char) => {
+    $(char).click(function () {
+        $(".active").removeClass('active');
+        $(char).addClass('active');
+    })
+})
+
+
+
+
+// slick slider
+$(".slider").slick({
+    centerMode: true,
+    centerPadding: "0px",
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    focusOnSelect: true,
+    prevArrow: "<img class='a-left control-c prev slick-prev' src='/img/btn-prev.png'>",
+    nextArrow: "<img class='a-right control-c next slick-next' src='/img/btn-next.png'>"
+});
+
+
+
+
+
